4cb03d624daba68f8c1fb1cdde9bf30c39804251
levelKro <levelkro@yahoo.ca>
2022-06-25 03:07:52 -0400

    Improvement
    Adding same result from Metric when errors.

--- temp/DS18B20/imperial.py
+++ temp/DS18B20/imperial.py
@@ -16,17 +16,23 @@ def read_temp_raw():
     return lines
  
 def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+    try:
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        output = str(round(temp_f,0)).replace(".0","") + degree_sign + "F"
-        #return output.encode("utf-8")
-        return output
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            degree_sign = u"\N{DEGREE SIGN}"
+            output = str(round(temp_f,0)).replace(".0","") + degree_sign + "F"
+            return output.encode("utf-8")
+            #return output
+    except:
+        return "I/O"
+    else:
+        return "I/O"
 	
 print(read_temp())	
